f8ea2015d1b44487b771360c97fd3acab99781b9
Dan <you@example.com>
2022-11-21 10:18:02 +0100

    Incartellati vari file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Ff1 from "./Final Fantasy/Ff1";
-import Ff2 from "./Final Fantasy/Ff2";
-import Ff3 from "./Final Fantasy/Ff3";
-import Ff4 from "./Final Fantasy/Ff4";
-import Ff5 from "./Final Fantasy/Ff5";
-import Ff6 from "./Final Fantasy/Ff6";
-import Ff7 from "./Final Fantasy/Ff7";
-import Ff8 from "./Final Fantasy/Ff8";
-import Ff9 from "./Final Fantasy/Ff9";
-import Ff10 from "./Final Fantasy/Ff10";
+import Ff1 from "./Final Fantasy/FF1/Ff1";
+import Ff2 from "./Final Fantasy/FF2/Ff2";
+import Ff3 from "./Final Fantasy/FF3/Ff3";
+import Ff4 from "./Final Fantasy/FF4/Ff4";
+import Ff5 from "./Final Fantasy/FF5/Ff5";
+import Ff6 from "./Final Fantasy/FF6/Ff6";
+import Ff7 from "./Final Fantasy/FF7/Ff7";
+import Ff8 from "./Final Fantasy/FF8/Ff8";
+import Ff9 from "./Final Fantasy/FF9/Ff9";
+import Ff10 from "./Final Fantasy/FF10/Ff10";
 
 function App() {
   return (
